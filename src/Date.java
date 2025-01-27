7e678cc26c8e92a495a89e8ac4ae77bb32ecd8bc
Zack <zacharycollier36@protonmail.com>
2025-01-27 12:54:03 -0330

    Added deliverable 2 - Date.java - DateTest.java - Deliverable 2 Screenshot - Removed Main.java

---
+++ src/Date.java
@@ -0,0 +1,42 @@
+public class Date {
+    //attributes
+    private int day;
+    private int month;
+    private int year;
+
+    //methods
+    public Date(int day, int month, int year){
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+
+    public int getDay(){
+        return this.day;
+    }
+    public int getMonth(){
+        return this.month;
+    }
+    public int getYear(){
+        return this.year;
+    }
+
+    public void setDay(int day){
+        this.day = day;
+    }
+    public void setMonth(int month){
+        this.month = month;
+    }
+    public void setYear(int year){
+        this.year = year;
+    }
+    public void setDate(int day, int month, int year){
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+
+    public String toString() {
+        return String.format("%02d/%02d/%d", this.day, this.month, this.year);
+    }
+}
---
+++ src/DateTest.java
@@ -0,0 +1,8 @@
+public class DateTest {
+    public static void main(String[] args) {
+
+        Date date = new Date(8, 12, 2019);
+
+        System.out.println(date.toString());
+    }
+}
